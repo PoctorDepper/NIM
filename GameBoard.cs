f532350b61eda1cf27fee96282bc03e271bf1764
PoctorDepper <nathille333@gmail.com>
2025-10-22 16:16:32 -0500

    Created GameBoard

---
+++ GameBoard.cs
@@ -0,0 +1,22 @@
+namespace NIM;
+
+public class GameBoard
+{
+    public int[] Piles { get; private set; }
+
+    public GameBoard(int pileCount, int pileSize)
+    {
+        Piles = new int[pileCount];
+        for (int pile = 0; pile < pileCount; ++pile)
+        {
+            Piles[pile] = pileSize;
+        }
+    }
+
+    public bool PlayMove(int pile, int amount)
+    {
+        if (pile >= Piles.Length || amount > Piles[pile]) return false;
+        Piles[pile] -= amount;
+        return true;
+    }
+}
