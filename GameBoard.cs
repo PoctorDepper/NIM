40c785ae62bf3c3693bda432d1233f52c935405e
PoctorDepper <nathille333@gmail.com>
2025-10-23 16:18:37 -0500

    I re-read the technical requirements, so I changed the piles from an array to a List. I also ended up adding documentation to the variables.

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,30 +1,34 @@
 namespace NIM;
 
-public class GameBoard
+public class GameBoard(int pileCount, int pileSize)
 {
-    public int[] Piles { get; private set; }
+    /// <summary>
+    /// The state of the piles in the game at the current moment.
+    /// </summary>
+    public List<int> Piles { get; } = Enumerable.Repeat(pileSize, pileCount).ToList();
 
-    public GameBoard(int pileCount, int pileSize)
-    {
-        Piles = new int[pileCount];
-        for (int pile = 0; pile < pileCount; ++pile)
-        {
-            Piles[pile] = pileSize;
-        }
-    }
-
+    /// <summary>
+    /// Plays a move on the current board, regardless of the player who played it.
+    /// </summary>
+    /// <param name="pile">Which pile to pick from.</param>
+    /// <param name="amount">How much to pick off of the selected pile.</param>
+    /// <returns><c>true</c> if the move was valid.</returns>
     public bool PlayMove(int pile, int amount)
     {
-        if (pile >= Piles.Length || amount > Piles[pile]) return false;
+        if (pile >= Piles.Count || amount > Piles[pile]) return false;
         Piles[pile] -= amount;
         return true;
     }
 
+    /// <summary>
+    /// Displays the game board as rows of piles with the value of that pile to the right of the index.
+    /// </summary>
+    /// <returns>The formatted string.</returns>
     public override string ToString()
     {
         string result = "";
 
-        for (var pile = 0; pile < Piles.Length; ++pile)
+        for (var pile = 0; pile < Piles.Count; ++pile)
         {
             result += $"{pile}: {Piles[pile]}\n";
         }
--- TreeNode.cs
+++ TreeNode.cs
@@ -1,15 +1,25 @@
 namespace NIM;
 
-public class TreeNode
+public class TreeNode(int player, List<int> piles)
 {
-    public int player = 1; //im thinking it will be 1 or 2 for players 3 for ai
-    public List<TreeNode> children;
-    public int[] piles;
+    /// <summary>
+    /// The player in which this node is for.
+    /// </summary>
+    public int Player { get; set; } = player; // im thinking it will be 1 or 2 for players 3 for ai
 
-    public TreeNode(int player, int[] piles)
-    {
-        this.player = player; //update player for this node should change per node
-        this.piles = piles; //current state of piles. So this should be a copy not a reference
-        children = new List<TreeNode>();
-    }
+    /// <summary>
+    /// The current state of the piles.
+    /// </summary>
+    public List<int> Piles { get; set; } = piles; // Current state of piles. So this should be a copy not a reference
+
+    /// <summary>
+    /// The likelihood of this <c>Player</c> to win.
+    /// </summary>
+    /// <value>Positive for this <c>Player</c>, negative for the opponent.</value>
+    public int MinimaxScore { get; set; }
+
+    /// <summary>
+    /// This node's children nodes.
+    /// </summary>
+    public List<TreeNode> Children { get; private set; } = [];
 }
